7a55b9838de6e7c7e305eb9aa2780879f27d0641
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-07-10 01:26:49 +0530

    Updated files

---
+++ main.java
@@ -0,0 +1,25 @@
+
+
+  class multiplethreads extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Task-1");
+    }
+}
+
+ class mythreads2 extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Task-2");
+    }
+}
+
+
+public class main{
+    public static void main(String[] args) {
+        multiplethreads t1 = new multiplethreads();
+        t1.start();
+        mythreads2 t2 = new mythreads2();
+        t2.start();
+    }
+}
